fd478a4020b81f8db4f6889a952df4f65cf81897
Christy <appletoms99@outlook.com>
2020-12-10 20:49:33 -0600

    create-react-app starter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, Route, Switch } from 'react-router-dom';
+import Draggable from 'react-draggable';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+  <div>
+
+  <header>
+    header
+
+    <Draggable>
+      <div>I can now be moved around!</div>
+    </Draggable>
+
+  </header>
+
+  <main>
+
+  </main>
+  
+  <footer>
+
+
+
+  </footer>
+  
+  </div>
   );
 }
 
