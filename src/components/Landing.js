eb7b37bec7c7ba10f228eec49d6b8e7a17ba5809
Christy <appletoms99@outlook.com>
2020-12-12 11:34:54 -0600

    add link to destinations on landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Map from '../assets/images/landing/world.png';
+import landing from '../landing.css'
 
 const Landing = () => {
     return (
+        <a href="/destinations">
         <img className="img-world" src={Map} alt="world map" />
+        </a>
+
     )
 }
 
