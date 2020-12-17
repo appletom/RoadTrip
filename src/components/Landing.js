8f8e44f41eaf09c1280842afb19f7c1cca78e824
Christy <appletoms99@outlook.com>
2020-12-16 20:14:31 -0600

    style landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Map from '../assets/images/landing/world.png';
+import cloud from '../assets/images/landing/a_cloud.png';
+import clouds from '../assets/images/landing/clouds.png';
 import styled, { keyframes } from 'styled-components';
 //import { bounce } from 'react-animations';
 //import Zoom from './Zoom';
@@ -10,10 +12,13 @@ animation: 2s ${keyframes`{bounce}`} infinite`;
 const Landing = () => {
     return (
         <div>
+        <img className="img-clouds" src={clouds} alt="clouds" />
 
         <a href="/destinations">
         <img className="img-world" src={Map} alt="world map" />
+
         </a>
+
 
 {/*         <Zoom>
         <h1>ROAD TRIP</h1>
